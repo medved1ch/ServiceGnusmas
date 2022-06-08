cb13400f90ef5a780b8abfbfd15fcf395d20f740
medved1ch <78206060+medved1ch@users.noreply.github.com>
2022-06-08 11:36:41 +0300

    Добавьте файлы проекта.

---
+++ ServiceGnusmas/Class/ConnectionDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceGnusmas.Class
+{
+    class ConnectionDB
+    {
+        public static string conn =
+                  "Data Source=192.168.50.154;" +
+                  "Initial Catalog=ServiceSAM;" +
+                  "User Id=admin;" +
+                  "Password=admin;";
+    }
+}
